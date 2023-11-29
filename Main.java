d802a08c3c8351a796f215e3faa33180efebe789
Sviat <slsvyatko@gmail.com>
2023-11-29 12:02:20 +0100

    added working main with sout

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("January, February, March, April, May, June, July, August, September, October, November, December");
-    }
-}
-
-
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("January, February, March, April, May, June, July, August, September, October, November, December");
+    }
 }
