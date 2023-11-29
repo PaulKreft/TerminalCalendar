bebba8425a24fd3345f24d343a859a536acdc28a
Sviat <slsvyatko@gmail.com>
2023-11-29 12:00:30 +0100

    added main method

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("January, February, March, April, May, June, July, August, September, October, November, December");
+    }
+}
+
+
